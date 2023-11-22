cb011449b928631e9e117909045363c9a8c43707
Feature <422375542@qq.com>
2023-11-22 13:40:21 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ FROM node:latest
 # 创建并设置应用的工作目录
 WORKDIR /app
 
+# 安装 ping、vim 和 npm
+RUN apt-get update && \
+    apt-get install -y iputils-ping vim
+
 # 复制 package.json 和 package-lock.json（如果存在）
 COPY package*.json ./
 
@@ -16,9 +20,5 @@ COPY . .
 # 应用在容器内监听的端口
 EXPOSE 3100
 
-# 定义环境变量，例如：数据库地址、端口号等（根据实际情况添加）
-# ENV DB_HOST=localhost
-# ENV DB_PORT=27017
-
 # 定义容器启动时执行的命令
-CMD ["node", "app.js"] # 假设您的启动文件是 index.js
+CMD ["node", "app.js"] # 替换为您的启动脚本
--- app.js
+++ app.js
@@ -4,66 +4,95 @@ const express = require('express');
 const axios = require('axios');
 const crypto = require('crypto');
 const fs = require('fs');
-
-// 从环境变量中获取 API 密钥和秘密密钥
-const apiKey = process.env.API_KEY;
-const secretKey = process.env.SECRET_KEY;
-
-console.log(`API key: ${apiKey}`);
-console.log(`secret key: ${secretKey}`);
+const sqlite3 = require('sqlite3').verbose();
 
 // 创建 Express 应用实例
 const app = express();
-const port = process.env.PORT;
+app.use(express.urlencoded({ extended: true })); // 用于解析 application/x-www-form-urlencoded
+
+// 设置端口
+const port = 3100;
+
+// 设置 json 文件路径
+const accountInfo_file = './data/accountInfo.json';
+const totalAssets_file = './data/totalAssets.json';
+
+// 连接 SQLite 数据库
+const db = new sqlite3.Database('./data/database.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the SQLite database.');
+});
+
+// 初始化数据库
+// db.serialize(() => {
+//     db.run(`CREATE TABLE IF NOT EXISTS keys (
+//         api_key TEXT NOT NULL,
+//         secret_key TEXT NOT NULL
+//     )`);
+// });
 
 // 延迟函数，遵守 API 频率限制
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
 
+// 获取密钥
+function getKeys() {
+    return new Promise((resolve, reject) => {
+        db.get("SELECT api_key, secret_key FROM keys ORDER BY rowid DESC LIMIT 1", (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row || {});
+            }
+        });
+    });
+}
+
 // 生成带签名的查询字符串
-function getQueryStringWithSignature(parameters) {
-    const queryString = Object.keys(parameters).map(key => key + '=' + parameters[key]).join('&');
-    const signature = crypto.createHmac('sha256', secretKey).update(queryString).digest('hex');
-    return queryString + '&signature=' + signature;
+async function getQueryStringWithSignature(parameters) {
+    const keys = await getKeys();
+    const queryString = Object.keys(parameters).map(key => `${key}=${parameters[key]}`).join('&');
+    const signature = crypto.createHmac('sha256', keys.secret_key).update(queryString).digest('hex');
+    return `${queryString}&signature=${signature}`;
 }
 
 // 获取指定币种的最新价格
 async function fetchPrice(asset) {
     try {
-        // 特殊处理，USDT 的价格始终设为 1
         if (asset === 'USDT') {
-            return '1';
+            return '1'; // USDT 的价格始终设为 1
         }
         const response = await axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${asset}USDT`);
         return response.data.price;
     } catch (error) {
         // console.error(`获取 ${asset} 价格时发生错误:`, error);
         console.error(`获取 ${asset} 价格时发生错误:`);
-
+        
         // 检查是否是无效交易对的错误
         if (error.response && error.response.data && error.response.data.code === -1121) {
             return 'N/A'; // 无效交易对，返回 'N/A'
         }
         return 'N/A'; // 其他错误，也返回 'N/A'
+
     }
 }
 
 // 获取币安账户信息并保存到本地文件
 async function fetchAndSaveAccountInfo() {
     try {
-        await delay(1000); // API 频率限制
+        await delay(1000); // 遵守 API 频率限制
 
-        const parameters = {
-            timestamp: Date.now(),
-            recvWindow: 10000
-        };
+        const parameters = { timestamp: Date.now(), recvWindow: 10000 };
+        const queryString = await getQueryStringWithSignature(parameters);
 
-        const queryString = getQueryStringWithSignature(parameters);
-        const headers = { 'X-MBX-APIKEY': apiKey };
+        const keys = await getKeys();
+        const headers = { 'X-MBX-APIKEY': keys.api_key };
+        const response = await axios.get(`https://api.binance.com/api/v3/account?${queryString}`, { headers });
 
-        const response = await axios.get(`https://api.binance.com/api/v3/account?${queryString}`, { headers: headers });
-        fs.writeFileSync('accountInfo.json', JSON.stringify(response.data));
+        fs.writeFileSync(accountInfo_file, JSON.stringify(response.data));
         console.log('账户信息已成功写入文件');
 
         let totalSum = 0;
@@ -78,7 +107,7 @@ async function fetchAndSaveAccountInfo() {
         }
 
         const today = new Date().toISOString().split('T')[0];
-        const totalAssetsFilePath = 'totalAssets.json';
+        const totalAssetsFilePath = totalAssets_file;
         let totalAssetsData = [];
 
         if (fs.existsSync(totalAssetsFilePath)) {
@@ -92,7 +121,7 @@ async function fetchAndSaveAccountInfo() {
             totalAssetsData.push({ date: today, total: totalSum.toFixed(2) });
         }
 
-        console.log('即将保存的每日资产总额数据：', totalAssetsData);
+        // console.log('即将保存的每日资产总额数据：', totalAssetsData);
         fs.writeFileSync(totalAssetsFilePath, JSON.stringify(totalAssetsData));
         console.log('每日资产总额已更新');
     } catch (error) {
@@ -100,19 +129,117 @@ async function fetchAndSaveAccountInfo() {
     }
 }
 
+// 检查数据库中是否存在有效密钥
+async function checkKeysExist(req, res, next) {
+    try {
+        console.log("检查密钥...");
+        const row = await getKeys();  // 使用 getKeys 函数获取密钥
+        console.log("从数据库获取的密钥：", row);
+
+        if (!row || !row.api_key || !row.secret_key) {
+            console.log("未找到有效的密钥，重定向到首页...");
+            res.send(`
+                <p>未设置密钥。3 秒后将自动跳转到首页进行设置。</p>
+                <script>
+                    setTimeout(function() {
+                        window.location.href = '/';
+                    }, 3000);
+                </script>
+            `);
+        } else {
+            console.log("找到有效密钥，继续处理请求...");
+            next();
+        }
+    } catch (error) {
+        console.error('检查密钥时发生错误：', error);
+        res.status(500).send('检查密钥时发生内部错误');
+    }
+}
+
+
+// 定义根路由，用于设置 API 密钥和展示其他功能链接
+app.get('/', async (req, res) => {
+    try {
+        const keys = await getKeys();
+        let apiKeyPlaceholder = '';
+        let secretKeyPlaceholder = '';
+
+        // 检查是否已经存在 API 密钥和秘密密钥
+        if (keys.api_key) {
+            apiKeyPlaceholder = keys.api_key.slice(-10); // 获取 API 密钥的后 10 位
+        }
+        if (keys.secret_key) {
+            secretKeyPlaceholder = keys.secret_key.slice(-10); // 获取秘密密钥的后 10 位
+        }
+
+        // 构建 HTML 页面
+        let html = `<!DOCTYPE html>
+        <html><head><title>币安资产监控平台</title></head><body>
+        <h1>欢迎来到币安资产监控平台</h1>
+        <hr>
+        <form action="/setKeys" method="post">
+            API Key: <input type="text" name="apiKey" placeholder="${apiKeyPlaceholder}" onclick="this.placeholder = ''" /><br>
+            Secret Key: <input type="text" name="secretKey" placeholder="${secretKeyPlaceholder}" onclick="this.placeholder = ''" /><br>
+            <input type="submit" value="设置密钥" />
+            <button type="button" onclick="window.location.href='/clearKeys'">清除密钥</button>
+        </form>
+        <hr>
+        <p><a href="/getAccountInfo/position">查看账户信息</a></p>
+        <p><a href="/showChart">查看资产图表</a></p>
+        </body></html>`;
+
+        res.send(html);
+    } catch (error) {
+        console.error('在根路由处理时发生错误：', error);
+        res.status(500).send('服务器内部错误');
+    }
+});
+
+// 清除密钥的路由处理
+app.get('/clearKeys', async (req, res) => {
+    try {
+        // 执行删除密钥的 SQL 语句
+        await db.run("DELETE FROM keys");
+        
+        // 清除成功后，发送回应并自动跳转到首页
+        res.send(`
+            <p>密钥已清除。3 秒后将自动跳转到首页。</p>
+            <script>
+                setTimeout(function() {
+                    window.location.href = '/';
+                }, 3000);
+            </script>
+        `);
+    } catch (error) {
+        console.error('清除密钥时发生错误：', error);
+        res.status(500).send('服务器错误：无法清除密钥');
+    }
+});
+
+// 处理密钥设置请求，并在 3 秒后自动跳转
+app.post('/setKeys', (req, res) => {
+    const { apiKey, secretKey } = req.body;
+    db.run(`INSERT INTO keys (api_key, secret_key) VALUES (?, ?)`, [apiKey, secretKey], (err) => {
+        if (err) {
+            res.status(500).send('保存密钥时发生错误');
+            return console.error(err.message);
+        }
+        res.send(`密钥已更新，3 秒后自动跳转。<script>setTimeout(() => { window.location.href = '/'; }, 3000);</script>`);
+    });
+});
+
+// 使用中间件在路由处理之前检查密钥
+app.use('/getAccountInfo/position', checkKeysExist);
+app.use('/showChart', checkKeysExist);
 
 // 设置定时任务
-setInterval(fetchAndSaveAccountInfo, 3000); // 每小时执行一次
+setInterval(fetchAndSaveAccountInfo, 3000); // 每 3 秒执行一次
 
-// 定义根路由
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
 // 定义 '/getAccountInfo/position' 路由
 app.get('/getAccountInfo/position', async (req, res) => {
     try {
-        const accountData = JSON.parse(fs.readFileSync('accountInfo.json', 'utf8'));
+        const accountData = JSON.parse(fs.readFileSync(accountInfo_file, 'utf8'));
         const positions = accountData.balances.filter(balance => parseFloat(balance.free) > 0);
 
         let html = '<!DOCTYPE html><html><head><title>账户余额及价格</title></head><body>';
@@ -146,18 +273,20 @@ app.get('/getAccountInfo/position', async (req, res) => {
                 });
             }
         </script>`;
-        html += '</body></html>';
+        // html += '</body></html>';
+        html += '</body><p><a href="/">返回首页</a></p></html>';
         res.send(html);
     } catch (error) {
         console.error('读取账户信息时发生错误', error);
         res.status(500).send('读取账户信息时发生错误');
     }
+
 });
 
 // 显示图表 /showChart 路由
 app.get('/showChart', (req, res) => {
     try {
-        const totalAssetsData = JSON.parse(fs.readFileSync('totalAssets.json', 'utf8'));
+        const totalAssetsData = JSON.parse(fs.readFileSync(totalAssets_file, 'utf8'));
         const dates = totalAssetsData.map(entry => entry.date);
         let totals = totalAssetsData.map(entry => parseFloat(entry.total));
 
@@ -222,7 +351,8 @@ app.get('/showChart', (req, res) => {
                 }
             });
         </script>`;
-        html += '</body></html>';
+        // 在 HTML 中增加一个返回首页的链接        
+        html += '<p><a href="/">返回首页</a></p>';
         res.send(html);
     } catch (error) {
         console.error('生成图表时发生错误', error);
